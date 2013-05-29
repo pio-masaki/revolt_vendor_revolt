48e812f56e2847059958f81b7e12eb92e2f51191
pio_masaki <piomasaki@gmail.com>
2013-05-29 14:27:36 -0700

    Adding DLX and files

---
+++ vendorsetup.sh
@@ -0,0 +1,69 @@
+## Nexus
+# Nexus 4
+add_lunch_combo revolt_mako-userdebug
+# Nexus 7
+add_lunch_combo revolt_grouper-userdebug
+# Nexus 10
+add_lunch_combo revolt_manta-userdebug
+# Galaxy Nexus
+add_lunch_combo revolt_maguro-userdebug
+
+## Samsung Phones
+# Galaxy S2
+add_lunch_combo revolt_i9100-userdebug
+# Galaxy S2G
+add_lunch_combo revolt_i9100g-userdebug
+# Galaxy S3
+add_lunch_combo revolt_i9300-userdebug
+# Sprint Galaxy S3
+add_lunch_combo revolt_d2spr-userdebug
+# Epic 4G
+add_lunch_combo revolt_d710-userdebug
+# Galaxy S4
+add_lunch_combo revolt_i9500-userdebug
+# Galaxy S4 
+add_lunch_combo revolt_i9505-userdebug
+# T-Mobile Galaxy S4
+add_lunch_combo revolt_jfltetmo-userdebug
+# Canadian Galaxy S4
+add_lunch_combo revolt_jfltecan-userdebug
+# USCC Galaxy S4
+add_lunch_combo revolt_jflteusc-userdebug
+# Sprint Galaxy S4
+add_lunch_combo revolt_jfltespr-userdebug
+# Verizon Galaxy S4
+add_lunch_combo revolt_jfltevzw-userdebug
+# AT&T Galaxy S4
+add_lunch_combo revolt_jflteatt-userdebug
+
+## Samsung Phablets
+# Galaxy Note
+add_lunch_combo revolt_n7000-userdebug
+# Galaxy Note 2
+add_lunch_combo revolt_n7100-userdebug
+
+## Samsung Tablets
+# Galaxy Note 10.1
+add_lunch_combo revolt_n8000-userdebug
+# Galaxy Note 10.1 (US Variant)
+add_lunch_combo revolt_n8010-userdebug
+# Galaxy Note 10.1 (Variant)
+add_lunch_combo revolt_n8013-userdebug
+
+## Oppo
+# Find5
+add_lunch_combo revolt_find5-userdebug
+
+## HTC
+# One
+add_lunch_combo revolt_m7-userdebug
+
+## Sony
+# odin
+add_lunch_combo revolt_odin-userdebug
+# yuga
+add_lunch_combo revolt_yuga-userdebug
+
+## Asus
+# tf300t
+add_lunch_combo revolt_tf300t-userdebug
